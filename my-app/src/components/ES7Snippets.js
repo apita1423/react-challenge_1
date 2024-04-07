13589c32cad9304c81089cee57af1bc90b174c0e
Amanda <apita1423@outlook.com>
2024-04-07 20:46:33 +0100

    Add es7snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,63 @@
+// // rfce - Creates a React Functional Component 
+// import React from 'react'
+
+// function ES7Snippets1() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets1
+
+
+
+
+// // rafce - Creates a React Arrow Function Component
+// import React from 'react'
+
+// const ES7Snippets2 = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets2
+
+
+
+
+// //rce - Creates a React class component
+// import React, { Component } from 'react'
+
+// export class ES7Snippets3 extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets3
+
+
+
+
+// //rconst - Adds a default constructor for a class that contains props as arguments
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+
+// //clg 
+// console.log(first)
+
+
+
+
+// //clo
+// console.log('first', first)
